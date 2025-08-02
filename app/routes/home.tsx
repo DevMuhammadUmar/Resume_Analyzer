02de27614948db6e642fe120e46d3c42fb068484
MUHAMMAD UMAR <umarfarhan@proton.me>
2025-08-02 05:36:51 +0500

    homepage,navbar and resumecard implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "JobMatch Pro"},
+        {name: "description", content: "Turn your resume into your AI-driven competitive advantage"},
+    ];
 }
 
+
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Achieve Your Career Goals One Application at a time</h1>
+                <h2>Turn rejection into redirection, tracking into triumph</h2>
+            </div>
+            {resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>)}
+        </section>
+
+    </main>
 }
