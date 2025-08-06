37bb7e66a47c0401590f2a50ac0001b3596c0822
MUHAMMAD UMAR <umarfarhan@proton.me>
2025-08-07 04:33:35 +0500

    Final Commit App ready for deployment

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import {usePuterStore} from "~/lib/puter";
 
 function ResumeCard({resume: {id, companyName, jobTitle, feedback, imagePath}}: { resume: Resume }) {
+    const {fs} = usePuterStore();
+    const [resumeUrl, setResumeUrl] = React.useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if (!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+        loadResume();
+    }, [imagePath]);
+
     return (
         <Link className="resume-card animate-in fade-in duration-1000" to={`/resume/${id}`}>
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="text-black font-bold break-words">
+                    {companyName && <h2 className="text-black font-bold break-words">
                         {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-grey-500">
+                    </h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-grey-500">
                         {jobTitle}
-                    </h3>
+                    </h3>}
+                    {!companyName && !jobTitle && <h2 className="text-black font-bold break-words"> Resume </h2>}
+
 
                 </div>
                 <div className="flex-shrink=0">
@@ -20,16 +36,16 @@ function ResumeCard({resume: {id, companyName, jobTitle, feedback, imagePath}}: 
 
                 </div>
             </div>
-            <div className="gradient-border animate-in fade-in duration-1000">
+            {resumeUrl && ( <div className="gradient-border animate-in fade-in duration-1000">
 
-                <div className="w-full h-full" >
+                <div className="w-full h-full">
                     <img
-                    src={imagePath}
-                    alt="resume"
-                    className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                />
+                        src={resumeUrl}
+                        alt="resume"
+                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                    />
                 </div>
-            </div>
+            </div>)}
         </Link>
     );
 }
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,11 +1,90 @@
-import React from 'react'
-import {useParams} from "react-router";
+import React, {useEffect, useState} from 'react'
+import {Link, useNavigate, useParams} from "react-router";
+import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import Ats from "~/components/ATS";
+import Details from "~/components/Details";
+import ATS from "~/components/ATS";
+
+export const meta = () => (
+    [{title: 'JobMatch Pro | Review'},
+        {name: 'description', content: 'Detailed Overview of your Resume'},
+    ])
 
 const Resume = () => {
+    const {id}=useParams();
+    const {auth,isLoading,fs,kv}=usePuterStore();
+    const [imageUrl,setImageUrl]=useState('');
+    const [resumeUrl,setResumeUrl]=useState('');
+    const [feedback,setFeedback]=useState<Feedback | null>(null);
+    const navigate=useNavigate();
 
-   const {id}=useParams();
+    useEffect(() => {
+        if (!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume${id}`);
+    }, [isLoading]);
+
+    useEffect(() => {
+        const loadResume=async ()=>{
+            const resume=await kv.get(`resume:${id}`);
+            if(!resume)return;
+
+            const data=JSON.parse(resume);
+            const resumeBlob=await fs.read(data.resumePath);
+            if(!resumeBlob)return;
+
+            const pdfBlob=new Blob([resumeBlob],{type:'application/pdf'});
+            const resumeUrl=URL.createObjectURL(pdfBlob);
+            setResumeUrl(resumeUrl);
+
+            const imageBlob=await fs.read(data.imagePath);
+            if(!imageBlob)return;
+
+            const imageBlobUrl=URL.createObjectURL(imageBlob);
+            setImageUrl(imageBlobUrl);
+
+            setFeedback(data.feedback);
+
+        }
+        loadResume();
+
+    }, [id]);
+
+
+
     return (
-        <div>Resume{id}</div>
+
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+                <span className="text-gray-800 text-sm font-semibold">Back to Home</span>
+                </Link>
+            </nav>
+            <div className="flex flext-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:h=[90%] max-2xl: w-fit h-fit">
+                            <a href={resumeUrl} target="_blank" rel="noreferrer">
+                                <img src={imageUrl} alt="resume" className="w-full h-full rounded-2xl object-contain" />
+                            </a>
+                        </div>
+                    )}
+                </section>
+
+                <section className="feedback-section">
+                    <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+                    {feedback?(
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000 ">
+                            <Summary feedback={feedback}/>
+                            <ATS score={feedback.ATS.score||0} suggestions={feedback.ATS.tips|| []}/>
+                            <Details feedback={feedback}/>
+                        </div>
+                    ):(
+                        <img src="/images/resume-scan-2.gif" className="w-full" />
+                    )}
+                </section>
+            </div>
+        </main>
     )
 }
 
